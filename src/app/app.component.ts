98e9a13f7dacbfcef7f26771b2597b6e2e50173b
Luis <luis.f.blanco@gmail.com>
2017-02-26 17:18:50 -0600

    Finished chaper 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,66 +1,62 @@
 import { Component } from '@angular/core';
 
+import { Customer } from './model';
+
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `
-  <h1>{{name}}</h1>
-  <fieldset>    
-    <img [src]="image" />
-  </fieldset>
-  <label [style.color]="color">Favorite Color:</label>
-  <button (click)="clicked()">Toggle Color</button>
-  <select (change)="colorChange($event.target.value)">
-    <option>red</option>
-    <option>blue</option>
-    <option>green</option>
-  </select>
-  <div [hidden]="hideAddress">
-    <div>
-      <label for="street">Street</label>
-      <input type="text" id="street" value="{{street}}" />
-    </div>
-    <div>
-      <label for="city">City</label>
-      <input type="text" id="city" value="{{city}}" />
-    </div>
-    <div>
-      <label for="region">Region</label>
-      <select (change)="regionChange($event.target.value)">
-        <option>East</option>
-        <option>North</option>
-        <option>South</option>
-        <option>West</option>
-      </select>
-      <p>{{name}} is in the {{region}} region</p>
-    </div>
-  </div>
-  <div>
-    <button (click)="addressClick()">{{hideAddress ? 'Show' : 'Hide'}} Address</button>
-  </div>
-  `,
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  name = 'Some Guy';
-  image = 'favicon.ico';
-  color = 'red';
-  street = "123 Main Street";
-  city = "Austin";
-  region = "East";
+
+  regions: Array<string> = ['East', 'West', 'South', 'North', 'Midwest'];
+  states: Array<string> = ['California', 'Illinois', 'Jalisco', 'Quebec'];
+
+  customers: Customer[] = [
+    {
+      id: 1,
+      name: 'Alex Smith',
+      address: {
+        street: '123 Main Street',
+        city: 'Anytown',
+        state: 'California',
+        region: 'West'
+      }
+    },
+    {
+      id: 2,
+      name: 'Pierre Pasmal',
+      address: {
+        street: '456 Rue de Main',
+        city: 'Quebec City',
+        state: 'Quebec',
+        region: 'East'
+      }
+    },
+    {
+      id: 3,
+      name: 'Margarita Nadie',
+      address: {
+        street: '789 Calle Principal',
+        city: 'Guadalajara',
+        state: 'Jalisco',
+        region: 'South'
+      }
+    },
+    {
+      id: 4,
+      name: 'Katie O\'Leary',
+      address: {
+        street: '137 DeKoven Street',
+        city: 'Chicago',
+        state: 'Illinois',
+        region: 'Midwest'
+      }
+    },
+  ];
+
+  customer: Customer;
+  
   hideAddress = false;
-
-  regionChange(region: string) {
-    this.region = region;
-  }
-
-  addressClick() {
-    this.hideAddress = !this.hideAddress;
-  }
-
-  clicked() {
-    this.color = this.color === 'red' ? 'blue' : 'red';
-  }
-
-  colorChange(color: string) {
-    this.color = color;
-  }
 }
